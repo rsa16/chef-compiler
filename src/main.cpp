43f65e89cb2be38febec8ad650465ff49a6c5c6a
rsa16 <sipnet66@outlook.com>
2024-07-02 15:30:08 -0400

    add license and test argc

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+/*
+  Copyright (c) 2024, Rehan Ali
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+*/
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World" << "\n";
+int main(int argc, char *argv[]) {
+    std::cout << argv[argc - 1] << "\n";
+
+
     return 0;
 }
