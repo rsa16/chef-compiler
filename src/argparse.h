942513e13046fdb1ea96433fda4f094df0b17c8d
rsa16 <sipnet66@outlook.com>
2024-07-02 15:30:49 -0400

    create skeleton tree

---
+++ src/argparse.h
@@ -0,0 +1,18 @@
+/*
+  Copyright (c) 2024, Rehan Ali
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+*/
+
+#include <iostream>
+
+namespace chefc 
+{
+    class ArgumentParser {
+    public:
+        ArgumentParser();
+    };
+}
+
