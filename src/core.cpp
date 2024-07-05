5d5483af1f2399d7768f5d473830ce026e5b4787
Rehan Ali <sipnet66@outlook.com>
2024-07-05 18:08:23 -0400

    create skeleton core compiler

--- src/core.cpp
+++ src/core.cpp
@@ -5,3 +5,28 @@
   This source code is licensed under the BSD-style license found in the
   LICENSE file in the root directory of this source tree.
 */
+#include "core.h"
+
+namespace chefc {
+    namespace core {
+        Compiler::Compiler(args::Parser& parser)
+        {
+            std::cout << "[INFO]: " << "Setting up initial flags and values." << "\n";
+            setupCommandLine(parser);
+
+            m_debuggingEnabled = parser.getFlag("enable_debugging");
+            if (m_debuggingEnabled)
+                std::cout << "[DEBUG]: " << "Debugging is now enabled! Verbosity increased." << "\n";
+        }
+
+        void Compiler::setupCommandLine(args::Parser& parser)
+        {
+            parser.addArgument("programName");
+            parser.addOptions<chefc::args::Flag>({
+                { "enable_debugging", chefc::args::Flag {"W1", "warning_1", "Enables debugging for the robot."} }
+            });
+
+            parser.parse();
+        }
+    }
+}
--- src/core.h
+++ src/core.h
@@ -5,3 +5,19 @@
   This source code is licensed under the BSD-style license found in the
   LICENSE file in the root directory of this source tree.
 */
+
+#include "argparse.h"
+#include "lexer/utils.h"
+
+namespace chefc {
+    namespace core {
+        class Compiler {
+        public:
+            Compiler(args::Parser& parser);
+            void setupCommandLine(args::Parser& parser);
+
+        private:
+            bool m_debuggingEnabled;
+        };
+    }
+}
